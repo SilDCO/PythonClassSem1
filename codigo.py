6b7a213bb46940dd1eaabe370670608b957232f0
silviadanyelle@hotmail.com <silviadanyelle@hotmail.com>
2025-02-20 12:57:03 +0000

    my first commit

---
+++ codigo.py
@@ -0,0 +1,12 @@
+apple1 = int(input("Size of the First: "))
+apple2 = int(input("Size of the Second:"))
+apple3 = int(input("Size of the Third: "))
+
+if apple1 > apple3:
+    if apple1 > apple2:
+        print (apple1)
+    else:
+        print (apple2)
+else: 
+    print (apple3)
+
