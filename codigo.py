ed935eef65aaff25ff76b845a438bc7811ddb6cd
silviadanyelle@hotmail.com <silviadanyelle@hotmail.com>
2025-03-04 14:10:24 +0000

    Comparison operators class

--- codigo.py
+++ codigo.py
@@ -1,12 +1,17 @@
-apple1 = int(input("Size of the First: "))
-apple2 = int(input("Size of the Second:"))
-apple3 = int(input("Size of the Third: "))
+#prints the sentence "Yes - Spathiphyllum is the best plant ever!" to the screen if the inputted string is "Spathiphyllum" (upper-case)
+#prints "No, I want a big Spathiphyllum!" if the inputted string is "spathiphyllum" (lower-case)
+#prints "Spathiphyllum! Not [input]!" otherwise. Note: [input] is the string taken as input.
 
-if apple1 > apple3:
-    if apple1 > apple2:
-        print (apple1)
-    else:
-        print (apple2)
-else: 
-    print (apple3)
+plantName = input("Enter with the plant name:")
+
+if plantName == "Spathiphyllum":
+        print("Yes - Spathiphyllum is the best plant ever!")
+elif plantName == "spathiphyllum":
+        print("No, I want a big Spathiphyllum!")
+else:
+        print ("No Spathiphyllum! Not " + plantName + "!")
+
+
+
+
 
