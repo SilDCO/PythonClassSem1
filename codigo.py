f25b03a7adb2777bd97630970a846006ce2b777e
silviadanyelle@hotmail.com <silviadanyelle@hotmail.com>
2025-03-11 14:51:55 +0000

    Loop class!

--- codigo.py
+++ codigo.py
@@ -15,14 +15,36 @@ else:
 #if the citizen's income was not higher than 85,528 thalers, the tax was equal to 18% of the income minus 556 thalers and 2 cents (this was the so-called tax relief)
 #if the income was higher than this amount, the tax was equal to 14,839 thalers and 2 cents, plus 32% of the surplus over 85,528 thalers.
 
-income = float(input("Enter the annual income: "))
+#income = float(input("Enter the annual income: "))
 
-if income <= 85_528:
-    tax = income*.18 - 556.02
-else: 
-    tax = (income-85_528)*.32+14_439.02
-if tax < 0:
-    tax= 0.0
-tax = round(tax,0)
-print("The tax is:", tax, "thalers")
+#if income <= 85_528:
+    #tax = income*.18 - 556.02
+#else: 
+    #tax = (income-85_528)*.32+14_439.02
+#if tax < 0:
+    #tax= 0.0
+#tax = round(tax,0)
+#print("The tax is:", tax, "thalers")
+
+#largest_number = -999999999
+#number = int(input("Enter a number or type -1 to stop: "))
+#while number != -1:
+    #if number > largest_number:
+        #largest_number = number
+    #number = int(input("Enter a number os type -1 to stop: "))
+
+#print ("The largest number is: ", largest_number)
+
+#moneyInMywallet = 100.
+#while moneyInMywallet >= 0:
+        #drinkname = input("I will buy a drink, what do you want? ")
+        #drinkprice = float(input("How much is it: "))
+        #if drinkprice <= moneyInMywallet:
+            #moneyInMywallet -= 7.5
+            #print ("Here is your ", drinkname, "Enjoy")
+        #else:
+            #print ("Sorry, I can't afford anymore, let's go.")
+           # break
+
+#print ("I left the pub, the money left is: ", moneyInMywallet)
 
